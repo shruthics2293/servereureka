3a013d7b531c557fefcc575aba6b84bb5aed7f3f
M1040969 <M1040969@A2ML17353.mindtree.com>
2019-11-23 12:52:22 +0530

    eurekhapush

---
+++ OrderEurekaServer/src/test/java/com/order/OrderEurekaServer/OrderEurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.order.OrderEurekaServer;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class OrderEurekaServerApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
